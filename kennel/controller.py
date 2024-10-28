175c43de2f8347f0d1ea230a5aa82208d886b359
Momotov Kirill <unknownman@live.ru>
2024-10-29 02:46:21 +0300

    From file based to sql based with SQLAlchemy

--- kennel/controller.py
+++ kennel/controller.py
@@ -2,7 +2,6 @@ from abc import ABC
 from datetime import datetime
 
 from view import View
-from model import Animal
 
 class Controller(ABC):
 
@@ -45,7 +44,7 @@ class AppController(Controller):
         else:
             match option:
                 case 1:
-                    self.stack.append(AddAnimalController(self.view).start)
+                    pass
                 case 2:
                     pass
                 case 3:
@@ -57,71 +56,3 @@ class AppController(Controller):
                                'используйте другие функции.')
                     self.view.show_message(message)
                     self.stack.append(self.main_menu)
-
-
-class AddAnimalController(Controller):
-
-    def __init__(self, view: View = None):
-        super().__init__(view)
-        self.new_animal = Animal(None, None, list())
-        self.stack.extend(
-            [
-                self.save_to_server,
-                self.get_list_of_commands,
-                self.get_date_of_birth,
-                self.get_name
-            ]
-        )
-
-    def get_name(self) -> None:
-        intro = 'Введите имя животного:'
-        self.new_animal.name = self.view.show_dialog(intro)
-
-    def get_date_of_birth(self) -> None:
-        intro = 'Введите дату рождения животного в формате дд.мм.гггг:'
-        user_choice = self.view.show_dialog(intro)
-        try:
-            self.new_animal.date_of_birth = datetime.strptime(user_choice, '%d.%m.%Y').date()
-        except ValueError:
-            message = ('ОШИБКА ВВОДА:\n-> Пожалуйста, укажите дату рождения в формате дд.мм.гггг, где:\n'
-                       '\t- дд - это день месяца с лидирующим нулём, например 01, 02...30, 31;\n'
-                       '\t- мм - это месяц с лидирующим нулём, например 01, 02...11, 12;\n'
-                       '\t- гггг - это год по грегорианскому календарю, например 1777, 1778...2023, 2024;\n'
-                       '\t- "." - это разделитель частей даты рождения.')
-            self.view.show_message(message)
-            self.stack.append(self.get_date_of_birth)
-
-    def get_list_of_commands(self) -> None:
-        first_loop = not bool(self.new_animal.commands)
-        if first_loop:
-            intro: str = 'Животное обучено каким-то командам? Хотите внести их в карточку?'
-        else:
-            intro: str = 'Внести еще одну команду в карточку животного?'
-        menu: list = ['Да', 'Нет']
-        user_choice = self.view.show_menu(intro, menu)
-        try:
-            option = int(user_choice)
-        except ValueError:
-            message = 'ОШИБКА ВВОДА:\n-> Пожалуйста, укажите номер нужной опции.'
-            self.view.show_message(message)
-            self.stack.append(self.get_list_of_commands)
-        else:
-            match option:
-                case 1:
-                    self.add_new_command()
-                    self.stack.append(self.get_list_of_commands)
-                case 2:
-                    pass
-                case _:
-                    message = ('ОШИБКА ВЫБОРА:\n-> Данный функционал еще не реализован, пожалуйста, '
-                               'используйте другие функции.')
-                    self.view.show_message(message)
-                    self.stack.append(self.get_list_of_commands)
-
-    def add_new_command(self) -> None:
-        intro = ('Вводите по 1-й команде за раз, иначе данные будут искаженны, спасибо за понимание.\n'
-                   'Укажите команду, которой обучено животное:')
-        self.new_animal.commands.append(self.view.show_dialog(intro))
-
-    def save_to_server(self):
-        print(self.new_animal)
